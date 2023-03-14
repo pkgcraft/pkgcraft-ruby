f2f01ccb962687133962e28c0437d810d141f938
Tim Harder <radhermit@gmail.com>
2023-03-13 23:25:58 -0600

    minor module import simplification

--- lib/pkgcraft.rb
+++ lib/pkgcraft.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "_pkgcraft_c"
 require_relative "pkgcraft/dep"
 require_relative "pkgcraft/_version"
 
--- lib/pkgcraft/dep.rb
+++ lib/pkgcraft/dep.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require "ffi"
-
-require_relative "../_pkgcraft_c"
 require_relative "dep/cpv"
 require_relative "dep/pkg"
 require_relative "dep/version"
