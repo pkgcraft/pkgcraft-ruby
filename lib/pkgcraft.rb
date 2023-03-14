4ee157e12d32f33f2b6c00e3810f5697483405e6
Tim Harder <radhermit@gmail.com>
2023-03-14 09:46:21 -0600

    move all module loading to the main module

--- lib/pkgcraft.rb
+++ lib/pkgcraft.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require "pkgcraft/bindings"
-require "pkgcraft/dep"
+require "pkgcraft/dep/cpv"
+require "pkgcraft/dep/pkg"
+require "pkgcraft/dep/version"
 require "pkgcraft/eapi"
 require "pkgcraft/error"
 require "pkgcraft/_version"
--- lib/pkgcraft/dep.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "dep/cpv"
-require_relative "dep/pkg"
-require_relative "dep/version"
