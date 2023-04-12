fbb03461dfe23f67a3f6ba9e5c701975f3841964
Tim Harder <radhermit@gmail.com>
2023-04-12 16:14:27 -0600

    repo: drop unnecessary Pkgcraft::Error include for temp repo support

--- lib/pkgcraft/repo/ebuild_temp.rb
+++ lib/pkgcraft/repo/ebuild_temp.rb
@@ -4,12 +4,10 @@ module Pkgcraft
   module Repos
     # Temporary ebuild package repo.
     class EbuildTemp < Ebuild
-      include Pkgcraft::Error
-
       def initialize(id = "test", eapi = EAPI_LATEST_OFFICIAL)
         eapi = Eapi.from_obj(eapi)
         ptr = C.pkgcraft_repo_ebuild_temp_new(id, eapi.ptr)
-        raise PkgcraftError if ptr.null?
+        raise Error::PkgcraftError if ptr.null?
 
         @ptr_temp = FFI::AutoPointer.new(ptr, C.method(:pkgcraft_repo_ebuild_temp_free))
         path = C.pkgcraft_repo_ebuild_temp_path(@ptr_temp)
@@ -20,7 +18,7 @@ module Pkgcraft
         ptr = FFI::MemoryPointer.new(:pointer, keys.length)
         ptr.write_array_of_pointer(keys.map { |s| FFI::MemoryPointer.from_string(s) })
         path, c_str = C.pkgcraft_repo_ebuild_temp_create_ebuild(@ptr_temp, cpv, ptr, keys.length)
-        raise PkgcraftError if c_str.null?
+        raise Error::PkgcraftError if c_str.null?
 
         C.pkgcraft_str_free(c_str)
         path
