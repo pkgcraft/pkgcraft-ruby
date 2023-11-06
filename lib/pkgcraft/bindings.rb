bcbcbf00cd3ba9cbd4e606910a91c9fc1a6f9d19
Tim Harder <radhermit@gmail.com>
2023-11-06 09:08:33 -0700

    bump pkgcraft-c compat versions

--- lib/pkgcraft/bindings.rb
+++ lib/pkgcraft/bindings.rb
@@ -6,8 +6,8 @@ module Pkgcraft
   # FFI bindings for pkgcraft
   module C
     # version requirements for pkgcraft-c
-    MIN_VERSION = "0.0.12"
-    MAX_VERSION = "0.0.12"
+    MIN_VERSION = "0.0.13"
+    MAX_VERSION = "0.0.13"
 
     extend FFI::Library
     ffi_lib ["pkgcraft"]
